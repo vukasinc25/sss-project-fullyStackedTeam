8012e4cff038b064b2281ab00ac2e8c619ab8d42
marko <crncevicmarko7@gmail.com>
2023-02-25 18:35:56 +0100

    appointment marko

---
+++ SSS-FullyStackedTeam/SSS-FullyStackedTeam/Service/AppointmentService.cs
@@ -0,0 +1,39 @@
+﻿using SSS_FullyStackedTeam.Model;
+using SSS_FullyStackedTeam.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSS_FullyStackedTeam.Service
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private IAppointmentRepository appointmentRepository;
+        public int Add(Appointment appointment)
+        {
+            return appointmentRepository.Add(appointment);
+        }
+
+        public void Delete(int id)
+        {
+            appointmentRepository.Delete(id);
+        }
+
+        public List<Appointment> GetAll()
+        {
+            return appointmentRepository.GetAll();
+        }
+
+        public Appointment GetById(int id)
+        {
+            return appointmentRepository.GetById(id);
+        }
+
+        public void Update(int id, Appointment appointment)
+        {
+            appointmentRepository.Update(id, appointment);
+        }
+    }
+}
---
+++ SSS-FullyStackedTeam/SSS-FullyStackedTeam/Service/IAppointmentService.cs
@@ -0,0 +1,18 @@
+﻿using SSS_FullyStackedTeam.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSS_FullyStackedTeam.Service
+{
+    internal interface IAppointmentService
+    {
+        List<Appointment> GetAll();
+        Appointment GetById(int id);
+        int Add(Appointment appointment);
+        void Update(int id, Appointment appointment);
+        void Delete(int id);
+    }
+}
