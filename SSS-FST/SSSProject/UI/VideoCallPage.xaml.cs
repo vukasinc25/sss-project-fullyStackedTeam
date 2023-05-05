b1e6eb5780b7ab232d16c0927499b2603e94a4b3
marko <crncevicmarko7@gmail.com>
2023-05-05 18:00:06 +0200

    video call page

---
+++ SSS-FST/SSSProject/UI/VideoCallPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SSSProject.UI
+{
+    /// <summary>
+    /// Interaction logic for VideoCallPage.xaml
+    /// </summary>
+    public partial class VideoCallPage : Page
+    {
+        private Window Window { get; set; }
+        public VideoCallPage(MainWindow mainWindow)
+        {
+            InitializeComponent();
+            Window = mainWindow;
+        }
+    }
+}
